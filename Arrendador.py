e1088f46adf8693180cb3213ee6e558af48ea871
Davidx144 <ndaristizabala@unal.edu.co>
2018-03-29 00:01:10 -0500

    Update Arrendador.py

--- Arrendador.py
+++ Arrendador.py
@@ -1,5 +1,24 @@
 from Usuario import * 
 
 class Arrendador(Usuario):
+    _Arrendadores = []     #Lista de objetos
+    
     def _init_(self,identificacion,nombre,_password):
         super()._init_(self,identificacion,nombre,_password)
+
+    def _getArrendadores():
+        orderkeys = Usuario.getKeys()
+        for i in orderkeys:
+            if isinstance(Usuario.getUsuario(i),Arrendador):
+                Arrendador._Arrendadores.append(Usuario.getUsuario(i))
+
+    def mejoresArrendadores():      #Retorna una lista con los mejores arrendadres de manera descendente
+        Arrendador._getArrendadores()
+        sorted(Arrendador._Arrendadores, key=lambda arrendador: Arrendador.calificacion)     #Ordena por el atributo calificacion
+        for i in range(0,len(Arrendador._Arrendadores)):
+            obj=Arrendador._Arrendadores[i]
+            aux1=obj.getNombre()
+            aux2=obj.getCalificacion()
+            listado.append(aux1)
+            listado.append(aux2)
+        return listado
